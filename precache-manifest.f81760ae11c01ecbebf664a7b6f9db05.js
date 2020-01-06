37bb36009eda81c80a2e6c8832504057a3c1b724
Aleksander Patschek <aleksander.patschek@hotmail.com>
2020-01-06 17:16:57 +0100

    Updates

---
+++ precache-manifest.f81760ae11c01ecbebf664a7b6f9db05.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "30a867aedb1d892d9fc61d4c1ef6a638",
+    "url": "/index.html"
+  },
+  {
+    "revision": "98472e4e3e622a8e9a7f",
+    "url": "/static/css/main.69eda509.chunk.css"
+  },
+  {
+    "revision": "257585975024e3aa6329",
+    "url": "/static/js/2.21cd6b4c.chunk.js"
+  },
+  {
+    "revision": "98472e4e3e622a8e9a7f",
+    "url": "/static/js/main.d91083a5.chunk.js"
+  },
+  {
+    "revision": "d01ad5ca3d6afd4a1145",
+    "url": "/static/js/runtime-main.99a54a36.js"
+  }
+]);
